a5b20c2dcb82a05b9794658cd9bf69358f6003d8
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-03-29 22:13:01 +0200

    343

--- Test/src/com/company/java/Main.java
+++ Test/src/com/company/java/Main.java
@@ -5,11 +5,15 @@ import com.company.java.Child;
 public class Main {
 
     public static void main(String[] args) {
-    Child child = new Child();
-    child.createChild();
+
+
+    //Child child = new Child();
+   // child.createChild();
         // Parent parent = new Parent();
     //parent.getParentData();
 
+        Database database = new Database();
+        database.getTeacherData();
 
 
     }
